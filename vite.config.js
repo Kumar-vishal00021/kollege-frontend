41ac53532a928143f5efbbd7cb40c4475ac0613c
Vishal Kumar <kumarvishal00021@gmail.com>
2025-11-08 00:17:22 +0530

    Fix: Add redirects for Render SPA routing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/',
   build: {
     chunkSizeWarningLimit: 1000, // in KB — default is 500
     outDir: 'dist'
